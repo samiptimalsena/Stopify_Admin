75d89623079831c60bc150018f0e460b232f6ce7
unknown <samip425@gmail.com>
2020-02-05 09:26:25 +0545

    credentials rechecked

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,14 @@ import  'firebase/storage'
 import 'firebase/firestore'
 
 const app=firebase.initializeApp({
-    apiKey:  "AIzaSyDyVwvz3K-QBehn9HEa161q9ACFQU8ohj0",
-    authDomain:  "auth-a3c8b.firebaseapp.com",
-    databaseURL:  "https://auth-a3c8b.firebaseio.com",
-    projectId:  "auth-a3c8b",
-    storageBucket:  "auth-a3c8b.appspot.com",
-    messagingSenderId:  "233330622166",
-    appId:  "1:233330622166:web:b0374f262ccaf0c46f1377"
+    apiKey: "AIzaSyDyVwvz3K-QBehn9HEa161q9ACFQU8ohj0",
+    authDomain: "auth-a3c8b.firebaseapp.com",
+    databaseURL: "https://auth-a3c8b.firebaseio.com",
+    projectId: "auth-a3c8b",
+    storageBucket: "auth-a3c8b.appspot.com",
+    messagingSenderId: "233330622166",
+    appId: "1:233330622166:web:b0374f262ccaf0c46f1377",
+    measurementId: "G-CF2V03GDYS"
   
 })
 
